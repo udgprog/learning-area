3f73dab5ba43af7ea2a34b147fdace5346bb0d4f
udgprog <47969238+udgprog@users.noreply.github.com>
2022-04-23 22:12:32 +0900

    Revert "support some random numbers"

--- javascript/introduction-to-js-1/first-splash/number-guessing-game.js
+++ javascript/introduction-to-js-1/first-splash/number-guessing-game.js
@@ -1,97 +1,27 @@
-let randomNumbers = new Array();
-let guesseses;
-let lastResults;
-let lowOrHis;
-let guessFields;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
 const timeLimit = document.querySelector('.timeLimit');
 let startButton = document.querySelector('.startButton');
 let guessSubmit = document.querySelector('.guessSubmit');
-let numbers = document.querySelector('.numbers');
-let quantity = document.querySelector('.quantity');
+let guessField = document.querySelector('.guessField');
 let guessCount = 1;
+let resetButton;
 let timer;
-let totalScore = document.querySelector('.totalScore');
-let score;
-let pause;
-const timeLimitConst = 100;
 
+guessField.disabled = true;
 
-function createNewNumber(){
-    let e=document.createElement("div");
-    e.className="number";
-
-    let form = document.createElement("div");
-	form.className="form";
-	e.appendChild(form);
-
-    let label = document.createElement("label");
-    label.setAttribute("for", "guessField");
-    label.textContent = "Enter a guess: "
-	form.appendChild(label);
-
-    let input = document.createElement("input");
-    input.type = "text";
-    input.id = "guessField";
-    input.className = "guessField";
-    form.appendChild(input);
-
-    let resultParas = document.createElement("div");
-    resultParas.className = "resultParas";
-
-    let child1 = document.createElement("p");
-    child1.className = "guesses";
-    let child2 = document.createElement("p");
-    child2.className = "lastResult";
-    let child3 = document.createElement("p");
-    child3.className = "lowOrHi";
-    resultParas.appendChild(child1);
-    resultParas.appendChild(child2);
-    resultParas.appendChild(child3);
-
-    e.appendChild(resultParas);
-    return e
-}
-
-
-//guessField.disabled = true;
-guessSubmit.disabled = true;
 startButton.addEventListener("click", startGame);
-pause = true;
-timer=setInterval("countDown()",1000);
 
 function startGame() {
-    let quantityValue = quantity.value;
-    quantityValue = parseInt(quantityValue)
-    if (!(quantityValue >= 1 && quantityValue <= 5)) {
-        alert("select number between 1 and 5");
-        return
-    }
-
-    score = 0;
-    totalScore.textContent = '';
-
-    for (let i = 0; i < quantityValue; i++) {
-        let newNumber = createNewNumber();
-        numbers.insertAdjacentElement('afterbegin', newNumber);
-        randomNumbers[i] = Math.floor(Math.random() * 100) + 1;
-    }
-    guesseses = document.querySelectorAll('.guesses');
-    lastResults = document.querySelectorAll('.lastResult');
-    lowOrHis = document.querySelectorAll('.lowOrHi');
-    guessFields = document.querySelectorAll('.guessField');
-    for (let i = 0; i < guessFields.length; i++) {
-        guessFields[i].addEventListener("input", stateHandle);
-    }
     startButton.disabled = true;
-    timeLimit.textContent = timeLimitConst;
-    //timer=setInterval("countDown()",1000);
-    pause = false;
+    guessField.disabled = false;
+    timeLimit.textContent = 100;
+    timer=setInterval("countDown()",1000);
 }
 
 function countDown() {
-    if (pause){
-        return
-    }
     let sec=timeLimit.textContent;
     sec=parseInt(sec);
     tmWrite(sec-1);
@@ -100,68 +30,25 @@ function countDown() {
 function tmWrite(int) {
     timeLimit.textContent=int;
     if (int<=0) {
-        for (let i = 0; i < guessFields.length; i++) {
-            let lastResult = lastResults[i];
-            let lowOrHi = lowOrHis[i];
-            lastResult.textContent = '!!!GAME OVER!!!';
-            lastResult.style.backgroundColor = 'red';
-            lowOrHi.textContent = '';
-        }
-        score = 0;
+        lastResult.textContent = '!!!GAME OVER!!!';
+        lastResult.style.backgroundColor = 'red';
+        lowOrHi.textContent = '';
         setGameOver();
     }
 }
 
-
-
-
+guessSubmit.disabled = true;
+guessField.addEventListener("input", stateHandle);
 
 function stateHandle() {
-    let emptyBool = false;
-    for (let i = 0; i < guessFields.length; i++) {
-        let guessField = guessFields[i];
-        if (guessField.disabled){
-            continue;
-        }
-        if(guessField.value === "") {
-            emptyBool = true;
-            continue;
-        }
-    }
-    if (emptyBool) {
+    if(guessField.value === "") {
         guessSubmit.disabled = true;
     } else {
         guessSubmit.disabled = false;
     }
 }
 
-function checkGuesses() {
-    let allRightFlag = true;
-    for (let i = 0; i < guesseses.length; i++) {
-        let guesses = guesseses[i];
-        let lastResult = lastResults[i];
-        let lowOrHi = lowOrHis[i];
-        let guessField = guessFields[i];
-        let randomNumber = randomNumbers[i];
-        if (!guessField.disabled){
-            let rightFlag = checkGuess(randomNumber, guesses, lastResult, lowOrHi, guessField);
-            if (!rightFlag){
-                allRightFlag = false;
-            }
-        }
-    }
-    guessSubmit.disabled = true;
-    if (allRightFlag){
-        setGameOver();
-    } else if (guessCount === 10){
-        score = 0;
-        setGameOver();
-    }
-    guessCount++;
-}
-
-function checkGuess(randomNumber, guesses, lastResult, lowOrHi, guessField) {
-    let rightFlag = false;
+function checkGuess() {
     const userGuess = Number(guessField.value);
     if (guessCount === 1) {
         guesses.textContent = 'Previous guesses: ';
@@ -170,19 +57,14 @@ function checkGuess(randomNumber, guesses, lastResult, lowOrHi, guessField) {
     guesses.textContent += userGuess + ' ';
 
     if (userGuess === randomNumber) {
-        let sec=timeLimit.textContent;
-        lastResult.textContent = 'Congratulations! You got it right! Score:' + sec;
+        lastResult.textContent = 'Congratulations! You got it right!';
         lastResult.style.backgroundColor = 'green';
         lowOrHi.textContent = '';
-        rightFlag = true;
-        guessField.disabled = true;
-        sec=parseInt(sec);
-        score += sec;
-        //setGameOver();
+        setGameOver();
     } else if (guessCount === 10) {
         lastResult.textContent = '!!!GAME OVER!!!';
         lowOrHi.textContent = '';
-        //setGameOver();
+        setGameOver();
     } else {
         lastResult.textContent = 'Wrong!';
         lastResult.style.backgroundColor = 'red';
@@ -193,60 +75,37 @@ function checkGuess(randomNumber, guesses, lastResult, lowOrHi, guessField) {
         }
     }
 
+    guessCount++;
     guessField.value = '';
     guessField.focus();
-    return rightFlag;
+    guessSubmit.disabled = true;
 }
 
-guessSubmit.addEventListener('click', checkGuesses);
+guessSubmit.addEventListener('click', checkGuess);
 
 function setGameOver() {
-    //clearInterval(timer);
-    pause = true;
-    for (let i = 0; i < guessFields.length; i++) {
-        let guessField = guessFields[i];
-        guessField.disabled = true;
-    }
+    clearInterval(timer);
+    guessField.disabled = true;
     guessSubmit.disabled = true;
-    startButton.disabled = false;
-    startButton.value = 'Start new game';
-    startButton.addEventListener('click', resetGame);
-    totalScore.textContent = String(score);
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
 }
 
 function resetGame() {
-    let quantityValue = quantity.value;
-    quantityValue = parseInt(quantityValue);
-    if (!(quantityValue >= 1 && quantityValue <= 5)) {
-        alert("select number between 1 and 5");
-        return
-    }
-
     guessCount = 1;
     const resetParas = document.querySelectorAll('.resultParas p');
     for (const resetPara of resetParas) {
         resetPara.textContent = '';
     }
 
-
-    /*
-    for (let i = 0; i < guessFields.length; i++) {
-        let randomNumber = Math.floor(Math.random() * 100) + 1;
-        randomNumbers[i] = randomNumber;
-        let guessField = guessFields[i];
-        guessField.disabled = false;
-        guessField.value = '';
-        guessField.focus();
-        let lastResult = lastResults[i];
-        lastResult.style.backgroundColor = 'white';
-    }
-    */
-    let numberDivs = document.querySelectorAll('.number');
-    for (let i = 0; i < numberDivs.length; i++) {
-        numberDivs[i].remove();
-    }
-    guessSubmit.disabled = true;
-    
-    
+    resetButton.parentNode.removeChild(resetButton);
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+    lastResult.style.backgroundColor = 'white';
+    randomNumber = Math.floor(Math.random() * 100) + 1;
     startGame();
 }
